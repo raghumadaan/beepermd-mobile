7425fee5fb4c787c94f5f6a9eeb6227584fd0599
devbeepermd <watchdeck.ios@gmail.com>
2023-03-20 20:38:49 +0530

    background service for ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import flutter_background_service_ios
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,17 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller = window.rootViewController as! FlutterViewController
+    let backgroundChannel = FlutterMethodChannel(name: "flutter_background_service", binaryMessenger: controller.binaryMessenger)
+    flutter_background_service.register(with: backgroundChannel)
+
+    let backgroundTask = UIApplication.shared.beginBackgroundTask(withName: "Background task") {
+        // End the background task if it hasn't finished by the time the app enters foreground again
+        UIApplication.shared.endBackgroundTask(backgroundTask)
+    }
+    flutter_background_service.start()
+
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
